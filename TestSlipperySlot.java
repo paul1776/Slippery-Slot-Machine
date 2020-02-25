1725f05aed0773372f6792af63286032aab00241
paul1776 <pec_2011@hotmail.com>
2020-02-25 18:24:37 -0500

    First test

---
+++ TestSlipperySlot.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.*;
+
+public class TestSlipperySlot {
+
+    @Test
+    public void thisTest() {
+        // First test
+        System.out.println("this");
+    }
+
+    @Test
+    public void thatTest() {
+        // Second test
+        System.out.println("that");
+    }
+
+    void main() {
+        System.out.println("Welcome to my Test file");
+    }
+}
