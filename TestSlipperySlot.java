30e56982b9d62ccfe4eb23d24eb1c1da9fef1ccb
paul1776 <pec_2011@hotmail.com>
2020-02-27 14:36:33 -0500

    JUnit Server Test #28

--- TestSlipperySlot.java
+++ TestSlipperySlot.java
@@ -141,7 +141,7 @@ public class TestSlipperySlot {
         assertTrue(test.payOff(fortyTwoOne) == 12);
     }
 
-    @Test (timeout = 50)
+    @Test (timeout = 0.15)
     public void testTimeout() {
         // Running payoff 20X to test timeout
         for (int i = 0; i < 21; i++) {
