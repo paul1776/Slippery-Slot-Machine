e8e51ea95c01d3280d92a6638a31faf18b85d4b5
paul1776 <pec_2011@hotmail.com>
2020-02-27 13:45:41 -0500

    JUnit Server Test #23

--- TestSlipperySlot.java
+++ TestSlipperySlot.java
@@ -58,6 +58,20 @@ public class TestSlipperySlot {
     }
 
     @Test
+    public void testOnlyOnePairAndFortyTwoOne() {
+        int[] pairAndFortyTwoOne = new int[]{3, 5, 7, 42, 42};
+        SlipperySlot test = new SlipperySlot();
+        assertTrue(test.payOff(pairAndFortyTwoOne) == 12);
+    }
+
+    @Test
+    public void testOnlyOnePairAndFortyTwoTwo() {
+        int[] pairAndFortyTwoTwo = new int[]{46, 41, 7, 7, 42};
+        SlipperySlot test = new SlipperySlot();
+        assertTrue(test.payOff(pairAndFortyTwoTwo) == 12);
+    }
+
+    @Test
     public void testAllFortyTwo() {
         int[] allFortyTwo = new int[]{42, 42, 42, 42, 42};
         SlipperySlot test = new SlipperySlot();
@@ -79,6 +93,13 @@ public class TestSlipperySlot {
     }
 
     @Test
+    public void testPerfectSquareAndPair() {
+        int[] perfectAndPair = new int[]{9,9,11,13,14};
+        SlipperySlot test = new SlipperySlot();
+        assertTrue(test.payOff(perfectAndPair) == 17);
+    }
+
+    @Test
     public void testAllSamePerfectSquare() {
         int[] allSamePerfect = new int[]{49, 49, 49, 49, 49};
         SlipperySlot test = new SlipperySlot();
@@ -87,7 +108,7 @@ public class TestSlipperySlot {
 
     @Test
     public void testPowerOfTwoOne() {
-        int[] powerOne = new int[]{3, 8, 16, 32, 33};
+        int[] powerOne = new int[]{3, 8, 11, 32, 33};
         SlipperySlot test = new SlipperySlot();
         assertTrue(test.payOff(powerOne) == 3);
     }
@@ -107,13 +128,6 @@ public class TestSlipperySlot {
     }
 
     @Test
-    public void testPerfectSquareAndPair() {
-        int[] perfectAndPair = new int[]{9,9,11,13,14};
-        SlipperySlot test = new SlipperySlot();
-        assertTrue(test.payOff(perfectAndPair) == 17);
-    }
-
-    @Test
     public void testAllSamePerfectSquareAndPowerOfTwo() {
         int[] allSamePerfect = new int[]{4, 4, 4, 4, 4};
         SlipperySlot test = new SlipperySlot();
@@ -125,19 +139,5 @@ public class TestSlipperySlot {
         int[] fortyTwoOne = new int[]{1, 4, 16, 42, 49};
         SlipperySlot test = new SlipperySlot();
         assertTrue(test.payOff(fortyTwoOne) == 12);
-    }
-
-    @Test
-    public void testOnlyOnePairAndFortyTwoOne() {
-        int[] pairAndFortyTwoOne = new int[]{3, 5, 7, 42, 42};
-        SlipperySlot test = new SlipperySlot();
-        assertTrue(test.payOff(pairAndFortyTwoOne) == 12);
-    }
-
-    @Test
-    public void testOnlyOnePairAndFortyTwoTwo() {
-        int[] pairAndFortyTwoTwo = new int[]{46, 41, 7, 7, 42};
-        SlipperySlot test = new SlipperySlot();
-        assertTrue(test.payOff(pairAndFortyTwoTwo) == 12);
     }
 }
