b4847aefce427e121df786f60f490b14a1d5440d
paul1776 <pec_2011@hotmail.com>
2020-02-27 11:47:42 -0500

    Testing whether JUnit works

--- TestSlipperySlot.java
+++ TestSlipperySlot.java
@@ -1,11 +1,51 @@
 import org.junit.jupiter.api.*;
+import org.junit.runner.RunWith;
+import org.junit.platform.runner.JUnitPlatform;
 
+@RunWith(Suite.class)
+@SuiteClasses({ SlipperySlot.class })
 public class TestSlipperySlot {
 
     @Test
+    public void testLeverPull() {
+        SlipperySlot test = new SlipperySlot();
+        int[] testLever = new int[];
+
+        // Produce 20 sets of random integers; verify all are between 1 and 50.
+        for (int i = 0; i < 21; i++) {
+            testLever = test.pullTheLever();
+            assertEquals(testLever.length, 5, "The lever pull must produce 5 integers");
+            assertTrue(0 < testLever[0] && testLever[0] < 51);
+            assertTrue(0 < testLever[1] && testLever[1] < 51);
+            assertTrue(0 < testLever[2] && testLever[2] < 51);
+            assertTrue(0 < testLever[3] && testLever[3] < 51);
+            assertTrue(0 < testLever[4] && testLever[4] < 51);
+        }
+    }
+
+    @Test
+    public void testFortyTwo() {
+
+    }
+
+    @Test
     public void allSame() {
-        // First test
-        System.out.println("this");
+        // Produce 20 sets of random identical integers between 1 and 50. Test with payout class.
+        for (int i = 0; i < 21; i++) {
+            // Generate a random number between 1 and 50.
+            int value = new Random().nextInt(50);
+            value += 1;
+
+            // Create an array of 5 of them
+            int[] testValues = new int[5];
+            for (int i = 0; i < 5; i++) {
+                testValues[i] = value;
+            }
+
+            // Test with payOff class
+            SlipperySlot test = new SlipperySlot();
+            assertTrue(test.payOff(testValues) > 1000000);
+        }
     }
 
     @Test
